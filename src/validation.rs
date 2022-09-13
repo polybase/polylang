d7e70d8b1b4da0fe46d147016717de8f6b887742
Mateusz Mielewczyk <mateuszmlc@protonmail.com>
2022-09-13 15:22:38 +0200

    Validate that data has no extra fields

--- src/validation.rs
+++ src/validation.rs
@@ -14,15 +14,19 @@ pub(crate) fn validate_set(
     collection: ast::Collection,
     data: HashMap<String, Value>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let fields = collection.items.iter().filter_map(|item| {
-        if let ast::CollectionItem::Field(field) = item {
-            Some(field)
-        } else {
-            None
-        }
-    });
+    let fields = collection
+        .items
+        .iter()
+        .filter_map(|item| {
+            if let ast::CollectionItem::Field(field) = item {
+                Some(field)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
 
-    for field in fields {
+    for field in &fields {
         let value = data.get(&field.name);
         if field.required && value.is_none() {
             return Err(format!("Missing required field: {}", field.name).into());
@@ -41,6 +45,12 @@ pub(crate) fn validate_set(
                     }
                 }
             }
+        }
+    }
+
+    for (key, _) in data {
+        if !fields.iter().any(|item| item.name == key) {
+            return Err(format!("Unexpected extraneous field: {}", key).into());
         }
     }
 
@@ -121,6 +131,33 @@ mod tests {
         let data = HashMap::from([
             ("name".to_string(), Value::Number(30.0)),
             ("age".to_string(), Value::String("30".to_string())),
+        ]);
+
+        assert!(validate_set(collection, data).is_err());
+    }
+
+    #[test]
+    fn test_validate_set_extra_field() {
+        let collection = ast::Collection {
+            name: "users".to_string(),
+            items: vec![
+                ast::CollectionItem::Field(ast::Field {
+                    name: "name".to_string(),
+                    type_: ast::Type::String,
+                    required: true,
+                }),
+                ast::CollectionItem::Field(ast::Field {
+                    name: "age".to_string(),
+                    type_: ast::Type::Number,
+                    required: false,
+                }),
+            ],
+        };
+
+        let data = HashMap::from([
+            ("name".to_string(), Value::String("John".to_string())),
+            ("age".to_string(), Value::Number(30.0)),
+            ("extra".to_string(), Value::String("extra".to_string())),
         ]);
 
         assert!(validate_set(collection, data).is_err());
