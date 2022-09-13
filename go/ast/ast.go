c9af883ac3373d244f1d0b86bc6152ff1d57fc8a
Mateusz Mielewczyk <mateuszmlc@protonmail.com>
2022-09-13 11:20:29 +0200

    Create AST structs in Go

---
+++ go/ast/ast.go
@@ -0,0 +1,56 @@
+package ast
+
+type Program struct {
+	Nodes []RootNode `json:"nodes"`
+}
+
+type RootNode struct {
+	Collection *Collection
+	Function   *Function
+}
+
+type Collection struct {
+	Name  string           `json:"name"`
+	Items []CollectionItem `json:"items"`
+}
+
+type CollectionItem struct {
+	Field    *Field    `json:"Field,omitempty"`
+	Function *Function `json:"Function,omitempty"`
+	Index    *Index    `json:"Index,omitempty"`
+}
+
+type Field struct {
+	Name     string `json:"name"`
+	Type     Type   `json:"type_"`
+	Required bool   `json:"required"`
+}
+
+type Type string
+
+const (
+	String Type = "String"
+	Number Type = "Number"
+)
+
+type Function struct {
+	Name       string        `json:"name"`
+	Parameters []string      `json:"parameters"`
+	Statements []interface{} `json:"statements"`
+}
+
+type Index struct {
+	Fields []IndexField `json:"fields"`
+}
+
+type IndexField struct {
+	Name  string `json:"name"`
+	Order Order  `json:"order"`
+}
+
+type Order string
+
+const (
+	Asc  Order = "Asc"
+	Desc Order = "Desc"
+)
