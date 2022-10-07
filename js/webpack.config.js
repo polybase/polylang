d81c2c16631e129d8b45363edb565ab8ff52f0e4
Mateusz Mielewczyk <mateuszmlc@protonmail.com>
2022-10-07 16:25:23 +0200

    Inline WASM to a base64 string

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 const nodeExternals = require('webpack-node-externals')
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
 
@@ -6,12 +7,12 @@ const config = (target) => ({
   target,
   entry: './src/index.ts',
   experiments: {
-    asyncWebAssembly: true,
+    topLevelAwait: true,
   },
   resolve: {
     extensions: ['.ts', '.js'],
   },
-  externals: [nodeExternals()],
+  externals: [nodeExternals(), { buffer: 'buffer' }],
   module: {
     rules: [
       {
@@ -30,6 +31,52 @@ const config = (target) => ({
       crateDirectory: path.resolve(__dirname, '..'),
       outDir: path.resolve(__dirname, 'pkg'),
     }),
+    {
+      apply: (compiler) => {
+        compiler.hooks.beforeCompile.tap('InlineWASM', () => {
+          const wasm = fs.readFileSync(
+            path.resolve(__dirname, 'pkg/index_bg.wasm'),
+          )
+
+          const js = `
+            import * as index_bg from "./index_bg.js"
+
+            const base64 = "${Buffer.from(wasm).toString('base64')}"
+
+            function toUint8Array (s) {
+              if (typeof atob === 'function') return new Uint8Array(atob(s).split('').map(c => c.charCodeAt(0)))
+              return (require('buffer').Buffer).from(s, 'base64')
+            }
+
+            const wasm = toUint8Array(base64)
+
+            const { instance } = await WebAssembly.instantiate(wasm, {
+              "./index_bg.js": index_bg,
+            })
+
+            export default instance.exports
+          `
+
+          fs.writeFileSync(path.resolve(__dirname, 'pkg/index_bg.wasm.js'), js)
+
+          const index = fs.readFileSync(
+            path.resolve(__dirname, 'pkg/index_bg.js'),
+          )
+
+          fs.writeFileSync(
+            path.resolve(__dirname, 'pkg/index_bg.js'),
+            index
+              .toString()
+              .replace(
+                'import * as wasm from \'./index_bg.wasm\'',
+                'import wasm from \'./index_bg.wasm.js\'',
+              ),
+          )
+
+          fs.unlinkSync(path.resolve(__dirname, 'pkg/index_bg.wasm'))
+        })
+      },
+    },
   ],
   output: {
     path: path.resolve(__dirname, target),
@@ -38,4 +85,4 @@ const config = (target) => ({
   },
 })
 
-module.exports = [config('node'), config('web')]
+module.exports = config(process.env.TARGET)
