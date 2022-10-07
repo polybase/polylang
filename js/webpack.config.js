4904c478289755e0384b832b9aa54fccdbd4cce0
mateuszmlc <110990416+mateuszmlc@users.noreply.github.com>
2022-10-07 17:05:17 +0200

    Inline WASM to base64, remove web/node split  (#20)
    * Inline WASM to a base64 string
    
    * Remove the split between web and node
    
    * Rename web to dist

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,17 +1,18 @@
 const path = require('path')
+const fs = require('fs')
 const nodeExternals = require('webpack-node-externals')
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
 
-const config = (target) => ({
-  target,
+module.exports = {
+  target: 'web',
   entry: './src/index.ts',
   experiments: {
-    asyncWebAssembly: true,
+    topLevelAwait: true,
   },
   resolve: {
     extensions: ['.ts', '.js'],
   },
-  externals: [nodeExternals()],
+  externals: [nodeExternals(), { buffer: 'buffer' }],
   module: {
     rules: [
       {
@@ -19,7 +20,7 @@ const config = (target) => ({
         loader: 'ts-loader',
         options: {
           compilerOptions: {
-            outDir: path.resolve(__dirname, target),
+            outDir: path.resolve(__dirname, 'dist'),
           },
         },
       },
@@ -30,12 +31,56 @@ const config = (target) => ({
       crateDirectory: path.resolve(__dirname, '..'),
       outDir: path.resolve(__dirname, 'pkg'),
     }),
+    {
+      apply: (compiler) => {
+        compiler.hooks.beforeCompile.tap('InlineWASM', () => {
+          const wasm = fs.readFileSync(
+            path.resolve(__dirname, 'pkg/index_bg.wasm'),
+          )
+
+          const js = `
+            import * as index_bg from "./index_bg.js"
+
+            const base64 = "${Buffer.from(wasm).toString('base64')}"
+
+            function toUint8Array (s) {
+              if (typeof atob === 'function') return new Uint8Array(atob(s).split('').map(c => c.charCodeAt(0)))
+              return (require('buffer').Buffer).from(s, 'base64')
+            }
+
+            const wasm = toUint8Array(base64)
+
+            const { instance } = await WebAssembly.instantiate(wasm, {
+              "./index_bg.js": index_bg,
+            })
+
+            export default instance.exports
+          `
+
+          fs.writeFileSync(path.resolve(__dirname, 'pkg/index_bg.wasm.js'), js)
+
+          const index = fs.readFileSync(
+            path.resolve(__dirname, 'pkg/index_bg.js'),
+          )
+
+          fs.writeFileSync(
+            path.resolve(__dirname, 'pkg/index_bg.js'),
+            index
+              .toString()
+              .replace(
+                'import * as wasm from \'./index_bg.wasm\'',
+                'import wasm from \'./index_bg.wasm.js\'',
+              ),
+          )
+
+          fs.unlinkSync(path.resolve(__dirname, 'pkg/index_bg.wasm'))
+        })
+      },
+    },
   ],
   output: {
-    path: path.resolve(__dirname, target),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     libraryTarget: 'commonjs2',
   },
-})
-
-module.exports = [config('node'), config('web')]
+}
