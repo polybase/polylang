0dc92a15444f7f20c63e9283788a69cb998d5daa
Mateusz Mielewczyk <mateuszmlc@protonmail.com>
2023-03-08 14:55:59 +0100

    Fix pull-collections script

--- pull-collections.sh
+++ pull-collections.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 mkdir -p test-collections
 
-collections="$(curl -L 'https://testnet.polybase.xyz/v0/collections/Collection/documents' | jq -r '.data | .[].data | @base64')"
+collections="$(curl -L 'https://testnet.polybase.xyz/v0/collections/Collection/records?limit=1000' | jq -r '.data | .[].data | @base64')"
 
 for collection in $collections; do
     collection="$(echo "$collection" | base64 -d)"
