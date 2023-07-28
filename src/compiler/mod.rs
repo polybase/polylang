9938c3935652a425454fb2cad76e1a7834d97c2c
Mateusz Mielewczyk <mateuszmlc@protonmail.com>
2023-07-28 17:38:54 +0200

    Implement public key literals
    commit b9e5675582bd311214ef0bf3828ee66107b3dd5c
    Merge: dc71868 9858a7d
    Author: Mateusz Mielewczyk <mateuszmlc@protonmail.com>
    Date:   Fri Jul 28 15:50:01 2023 +0200
    
        Merge branch 'miden-compiler' of github.com-mateuszmlc:polybase/polylang into eng-961-publickey-literals
    
    commit dc71868a0e7b1b9bf5376469ab19635559a5e53c
    Author: Mateusz Mielewczyk <mateuszmlc@protonmail.com>
    Date:   Fri Jul 21 15:19:36 2023 +0200
    
        Remove unwrap, print error for addresses in place of pk, print source for directive errors
    
    commit e3ef39bb452b0200c3190b5d549ce1df816078a7
    Author: Mateusz Mielewczyk <mateuszmlc@protonmail.com>
    Date:   Fri Jul 21 14:01:52 2023 +0200
    
        add PublicKey literals, accept them in auth
    
    commit b7fd8d962bd1b529a2abc11a3f230827f2ea79c8
    Author: Mateusz Mielewczyk <mateuszmlc@protonmail.com>
    Date:   Thu Jul 20 14:31:40 2023 +0200
    
        Add ethereum literal
    
    commit aa5b80db0e50ab05249d5231561995c2c5320fdb
    Author: Mateusz Mielewczyk <mateuszmlc@protonmail.com>
    Date:   Tue Jul 18 23:58:02 2023 +0200
    
        Run tests for all workspace packages in CI
    
    commit 72b19082ece80f03f79a8eaecfd8407b9f013dcd
    Author: Mateusz Mielewczyk <mateuszmlc@protonmail.com>
    Date:   Tue Jul 18 04:21:21 2023 +0200
    
        Return input_stack from prover
    
    commit 6a47608bbed83c71724bc9f94c17c204038a1b6a
    Author: Mateusz Mielewczyk <mateuszmlc@protonmail.com>
    Date:   Tue Jul 18 03:53:42 2023 +0200
    
        Replace unwraps with Try, fix eq in compile_check_ownership
    
    commit ca69be615b5246d3d359de50b283f5933ac4883a
    Author: Mateusz Mielewczyk <mateuszmlc@protonmail.com>
    Date:   Tue Jul 18 03:35:01 2023 +0200
    
        Add tests
    
    commit 2b2475856b6fd3c3a05d11387d3859c26f32baaa
    Author: Mateusz Mielewczyk <mateuszmlc@protonmail.com>
    Date:   Tue Jul 18 03:33:45 2023 +0200
    
        Fix compile errors after merge
    
    commit 07cc2de39ee4db8f1deae9325b82729869fccb8a
    Merge: 23270c5 09bf550
    Author: Mateusz Mielewczyk <mateuszmlc@protonmail.com>
    Date:   Tue Jul 18 03:17:23 2023 +0200
    
        Merge branch 'miden-compiler' of github.com-mateuszmlc:polybase/polylang into eng-527-miden-authority-ownership
    
    commit 23270c5df6a9137f55e9cc8c2ddf05c12b2aad70
    Author: Mateusz Mielewczyk <mateuszmlc@protonmail.com>
    Date:   Tue Jul 18 03:04:46 2023 +0200
    
        Implement read/auth proofs
    
    commit 536857d3d7f413244892dc47662c7554c9736872
    Author: Mateusz Mielewczyk <mateuszmlc@protonmail.com>
    Date:   Mon Jul 3 17:39:40 2023 +0200
    
        Upgrade to Miden v0.6.1

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -3746,12 +3746,11 @@ fn compile_call_authorization_proof(
         (false, false) => return Ok(result),
     }
 
-    let mut call_fields = call_decorators
-        .flat_map(|d| &d.arguments)
-        .map(|a| a.split('.').collect::<Vec<_>>())
+    let mut call_args = call_decorators
+        .flat_map(|d| d.arguments.iter().map(|a| (d.span(), a)))
         .peekable();
 
-    if function_has_call_directive && call_fields.peek().is_none() {
+    if function_has_call_directive && call_args.peek().is_none() {
         // The call is just `@call` with no fields, so no authorization required.
         compiler.instructions.push(encoder::Instruction::Push(1));
         compiler
@@ -3760,13 +3759,27 @@ fn compile_call_authorization_proof(
         return Ok(result);
     }
 
-    for call_field_path in call_fields {
-        let mut current_field = collection_symbol.clone();
-        for field in call_field_path {
-            current_field = struct_field(compiler, &current_field, field)?;
-        }
+    for (decorator_span, call_arg) in call_args {
+        maybe_start!(decorator_span);
 
-        let passed = compile_check_eq_or_ownership(compiler, current_field, auth_pk)?;
+        let arg_value = match call_arg {
+            ast::DecoratorArgument::Identifier(id) => {
+                let mut current_field = collection_symbol.clone();
+                for field in &[id] {
+                    current_field = struct_field(compiler, &current_field, field)?;
+                }
+
+                current_field
+            }
+            ast::DecoratorArgument::Literal(l) => match l {
+                ast::Literal::Eth(pk) => {
+                    let key = abi::publickey::Key::from_secp256k1_bytes(&pk).wrap_err()?;
+                    publickey::new(compiler, key)
+                }
+            },
+        };
+
+        let passed = compile_check_eq_or_ownership(compiler, arg_value, auth_pk)?;
         compiler.instructions.push(encoder::Instruction::If {
             condition: vec![encoder::Instruction::MemLoad(Some(passed.memory_addr))],
             then: vec![
