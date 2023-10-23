416b6d80c31cf13cf02aef4ceb72e3fb3b877c38
Timmy Jose <zoltan.jose@gmail.com>
2023-10-23 15:12:48 +0530

    ENG-1231: Add CORS to polylang-server (#135)
    * Changes:
      - set CORS for `polylang server` (prover) to "permissive".
      - added `overflow-addrs` field to `Output` in the prover (used by the
        verifier).`

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn prove(
@@ -11,7 +12,12 @@ async fn main() {
     let port = std::env::var("PORT").unwrap_or("8080".to_string());
     let listen_addr = std::env::var("PROVER_LADDR").unwrap_or(format!("0.0.0.0:{port}"));
 
-    let app = || App::new().service(web::resource("/prove").route(web::post().to(prove)));
+    let app = || {
+        let cors = Cors::permissive();
+        App::new()
+            .wrap(cors)
+            .service(web::resource("/prove").route(web::post().to(prove)))
+    };
 
     eprintln!("Listening on {}", listen_addr);
 
